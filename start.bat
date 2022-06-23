b116ea10fbe0df3b68361469d1e148e5bf2681d6
neo <neonilr1999@gmail.com>
2022-06-23 12:40:16 +0300

    modified start script

--- start.bat
+++ start.bat
@@ -5,7 +5,7 @@ cd..
 pip install -r requirements.txt
 cd MyCV
 START manage.py runserver
-timeout 10
-start chrome "http://127.0.0.1:8000/person/1#page-top"
+timeout 5
 start chrome "http://127.0.0.1:8000/person/2#page-top"
+start chrome "http://127.0.0.1:8000/person/1#page-top"
 timeout 10
